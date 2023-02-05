814511aae911d87735064cc346c2b370df3f1ab7
MRameur2022 <belkhiriaameur@gmail.com>
2023-02-05 07:41:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Menu from './Menu';
+import Cantact from './Cantact';
+import BasicExample from './BasicExemple';
+
+function App() {
+  return (
+    <div>
+      <BasicExample></BasicExample>
+      <Navbar></Navbar>
+      <Menu></Menu>
+      <Cantact></Cantact>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Cantact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cantact() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Cantact
